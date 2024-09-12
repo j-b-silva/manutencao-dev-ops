39fe848852b90e32e30415b733ef0a2ad172a89e
j-b-silva <julia.b.silva@iesb.edu.br>
2024-09-12 20:14:04 -0300

    Criando testes para a Calculadora

--- projeto-testes-jest/Calculadora.js
+++ projeto-testes-jest/Calculadora.js
@@ -2,6 +2,19 @@ class Calculadora{
     soma(a,b){
         return a+b;
     };
+
+    subtrai(a,b){
+        return a-b;
+    };
+
+    divide(a,b){
+        if(b===0) throw new Error('Divisão por zero não é permitida!');
+        return a/b;
+    };
+
+    multiplica(a,b){
+        return a*b;
+    };
 }
 
 module.exports = Calculadora;
--- projeto-testes-jest/Calculadora.test.js
+++ projeto-testes-jest/Calculadora.test.js
@@ -8,6 +8,22 @@ describe('Testes da Calculadora', ()=>{
     });
 
     test('Deve somar dois números', ()=>{
-        expect(calc.soma(2,3).toBe(5))
-    })
-})
+        expect(calc.soma(2,1)).toBe(3);
+    });
+
+    test('Deve subtrair dois número', ()=>{
+        expect(calc.subtrai(3,1)).toBe(2);
+    });
+
+    test('Deve dividir dois número', ()=>{
+        expect(calc.divide(2,2)).toBe(1);
+    });
+
+    test('Deve multiplicar dois número', ()=>{
+        expect(calc.multiplica(3,0)).toBe(0);
+    });
+
+    test('Deve lançar erro ao dividir por zero', ()=>{
+        expect(()=> calc.divide(2,0)).toThrow('Divisão por zero não é permitida!');
+    });
+});
